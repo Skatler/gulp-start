3100b9ffa1994eb9ad89d5eef4ad92c89349ee0d
Зозуля Ярослав <yaroslavzozuliya95@gmail.com>
2023-06-25 14:41:52 +0400

    базовая сборка GULP

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-const { src, dest, watch, parallel } = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
 
 function scripts() {
+  // что бы добавить другие JS файлы нужно
+  // return src(['другой файл', 'app/js/main.js'])
   return src('app/js/main.js')
     .pipe(concat('main.min.js'))
     .pipe(uglify())
@@ -15,6 +19,7 @@ function scripts() {
 
 function styles() {
   return src('app/scss/style.scss')
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
     .pipe(concat('style.min.css'))
     .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(dest('app/css'))
@@ -23,7 +28,7 @@ function styles() {
 
 function watching() {
   watch(['app/scss/*.scss'], styles);
-  watch(['app/js/main.js'], scripts);
+  watch(['app/js/*.js'], scripts);
   watch(['app/*.html']).on('change', browserSync.reload);
 }
 
@@ -35,9 +40,24 @@ function browsersync() {
   });
 }
 
+function cleanDist() {
+  return src('dist')
+    .pipe(clean());
+}
+
+function building() {
+  return src([
+    'app/css/style.min.css',
+    'app/js/main.min.js',
+    'app/**/*.html'
+  ], { base: 'app' })
+    .pipe(dest('dist'));
+}
+
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
 
+exports.build = series(cleanDist, building);
 exports.default = parallel(styles, scripts, browsersync, watching);
